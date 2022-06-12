f6122a4f178e98aa01b763162309053efae9735e
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2022-06-12 10:22:12 +0600

    Compleate

--- lib/view_screen/full_image_page.dart
+++ lib/view_screen/full_image_page.dart
@@ -8,22 +8,12 @@ import 'package:flutter_wallpaper_manager/flutter_wallpaper_manager.dart';
 class FullImagePage extends StatefulWidget {
   FullImagePage({Key? key, this.image}) : super(key: key);
   var image;
-  // imageUrl: widget.image,
+
   @override
   State<FullImagePage> createState() => _FullImagePageState();
 }
 
 class _FullImagePageState extends State<FullImagePage> {
-  // Future<void> setwallpaper() async {
-  //   int location = WallpaperManager.HOME_SCREEN;
-  //
-  //   var file = await DefaultCacheManager().getSingleFile(widget.image);
-  //   final String result =
-  //   (await WallpaperManager.setWallpaperFromFile(file.path, location)) as String;
-  // }
-
-
-
   Future<void> setWallpaper() async {
     try {
       String url = widget.image;
@@ -31,87 +21,40 @@ class _FullImagePageState extends State<FullImagePage> {
           .BOTH_SCREEN; // or location = WallpaperManager.LOCK_SCREEN;
       var file = await DefaultCacheManager().getSingleFile(url);
       final bool result =
-      await WallpaperManager.setWallpaperFromFile(file.path, location);
+          await WallpaperManager.setWallpaperFromFile(file.path, location);
       print(result);
     } on PlatformException {}
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
           child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  child:
-                  CachedNetworkImage(
-                    imageUrl: widget.image,
-                    errorWidget: (context, url, error) => Text("error"),
-                    // imageBuilder: (context, imageProvider) => CircleAvatar(
-                    //   radius: 120,
-                    //   backgroundImage: imageProvider,
-                    // ),
-                    // placeholder: (context, url) => CircularProgressIndicator(
-                    //
-                    // ),
-                  ),
-                  // Image.network(widget.image),
-                ),
-              ),
-              InkWell(
-                onTap: () {
+        children: [
+          Expanded(
+            child: CachedNetworkImage(
+              imageUrl: widget.image,
+              fit: BoxFit.fitHeight,
+              errorWidget: (context, url, error) => Text("error"),
+            ),
+          ),
+          InkWell(
+            onTap: () {
               setWallpaper();
-                },
-                child: Container(
-                  height: 60,
-                  width: double.infinity,
-                  color: Colors.black,
-                  child: Center(
-                    child: Text('Set Wallpaper',
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
-                  ),
-                ),
-              )
-            ],
-          )),
-
-
-
-
-      // Column(
-      //     children: [
-      //
-      //       Center(
-      //         child: CachedNetworkImage(
-      //           imageUrl: widget.image,
-      //           errorWidget: (context, url, error) => Text("error"),
-      //         ),
-      //
-      //         // Image.network(widget.image),
-      //       ),
-      //       InkWell(
-      //         onTap: (){
-      //   setWallpaper();
-      //         },
-      //         child: Container(
-      //           child: Container(
-      //             height: 60,
-      //             width: double.infinity,
-      //             color: Colors.black,
-      //             child: Center(
-      //               child: Text('Set Wallpaper',
-      //                   style: TextStyle(fontSize: 20, color: Colors.white)),
-      //             ),
-      //           ),
-      //
-      //         ),
-      //       ),
-      //     ],
-      //   ),
-
-
-
-
+            },
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              color: Colors.orange,
+              child: const Center(
+                child: Text('Set Wallpaper',
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
--- lib/view_screen/home_screen.dart
+++ lib/view_screen/home_screen.dart
@@ -16,16 +16,107 @@ class HomeView extends StatelessWidget {
           title: const Text('Wallpaper App'),
           centerTitle: true,
         ),
-        body: controller.obx(
+        body:
+
+//         controller.obx(
+//               (state) {
+//                 // leading:  fit: BoxFit.fill,),
+//                 // subtitle: Text("${item.height}"),
+//             return 
+//              
+//               ListView(
+//               children: List.generate(state?.length ?? 0, (index) {
+//                 final item = state![index];
+//                 return InkWell(
+//                   onTap: (){
+//                     if (index==0) {
+//                      Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==1) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==3) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==4) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==5) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==6) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==7) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==8) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==9) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==10) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==11) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==12) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==13) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==14) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==15) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//                     if (index==16) {
+//                       Get.to(FullImagePage(image: item.urls?.full,));
+//                     }
+//
+//                   },
+//                   child: Card(
+//                     child:CachedNetworkImage(
+//                       imageUrl: "${item.urls?.full}",
+//                       errorWidget: (context, url, error) => const Text("error"),
+//                       placeholder: (context, url) => const CircularProgressIndicator(
+//                       ),
+//                     ),
+//                     // child: Image.network("${item.urls?.full}",),
+//                   ),
+//                 );
+//               }),
+//             );
+//           },
+//           onLoading: const Center(child: CircularProgressIndicator()),
+//           onError: (error) => Center(child: Text(error.toString())),
+//           onEmpty: const Center(child: Text("No Data")),
+//
+//         )
+//     );
+//   }
+// }
+
+
+        controller.obx(
               (state) {
-                // leading:  fit: BoxFit.fill,),
-                // subtitle: Text("${item.height}"),
-            return ListView(
-              children: List.generate(state?.length ?? 0, (index) {
+            return GridView.extent(
+              primary: false,
+              padding: const EdgeInsets.all(16),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              maxCrossAxisExtent: 200.0,
+              children: List.generate(state?.length ?? 0, (index){
                 final item = state![index];
-                return InkWell(
-                  onTap: (){
-                    if (index==0) {
+                return SingleChildScrollView(
+                  child:
+                  InkWell(
+                    onTap: (){
+                      if (index==0) {
                      Get.to(FullImagePage(image: item.urls?.full,));
                     }
                     if (index==1) {
@@ -73,26 +164,76 @@ class HomeView extends StatelessWidget {
                     if (index==16) {
                       Get.to(FullImagePage(image: item.urls?.full,));
                     }
-
-                  },
-                  child: Card(
-                    child:CachedNetworkImage(
-                      imageUrl: "${item.urls?.full}",
-                      errorWidget: (context, url, error) => const Text("error"),
-                      placeholder: (context, url) => const CircularProgressIndicator(
+                      if (index==17) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==18) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==19) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==20) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==21) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==22) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==23) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==24) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==25) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==26) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==27) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==28) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==29) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                      if (index==30) {
+                        Get.to(FullImagePage(image: item.urls?.full,));
+                      }
+                    },
+                    child: Card(
+                      elevation: 5.0,
+                      shape: BeveledRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl: "${item.urls?.full}",
+                            errorWidget: (context, url, error) => Text("error"),
+                            placeholder: (context, url) => CircularProgressIndicator(
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    // child: Image.network("${item.urls?.full}",),
                   ),
                 );
-              }),
+
+                },
+              ),
             );
           },
           onLoading: const Center(child: CircularProgressIndicator()),
           onError: (error) => Center(child: Text(error.toString())),
           onEmpty: const Center(child: Text("No Data")),
-
-        )
-    );
+        ));
   }
 }
