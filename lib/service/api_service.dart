4c47efd59378d869f33a500e55d65e1cfc887ad6
Kawsar Ali <59552845+Kawsar07@users.noreply.github.com>
2022-06-12 10:23:20 +0600

    new added

--- lib/service/api_service.dart
+++ lib/service/api_service.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 
 import '../models/wallModels.dart';
+
 class RemoteService {
   Dio _dio = Dio();
   final Dio dio;
-  static const String url = "https://api.unsplash.com/photos?per_page=30&client_id=CxIbE8GyVA4qC0_X4UMbG2sdEASOCIe7cdOM8qZlPaA";
+  static const String url =
+      "https://api.unsplash.com/photos?per_page=30&client_id=CxIbE8GyVA4qC0_X4UMbG2sdEASOCIe7cdOM8qZlPaA";
 
   RemoteService(this.dio);
 
@@ -19,7 +21,4 @@ class RemoteService {
     }
     return data;
   }
-
-
-
 }
